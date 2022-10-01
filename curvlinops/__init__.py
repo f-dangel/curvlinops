754be5ab655003c0381662e3f823a7022a7f1302
Felix Dangel <fdangel@tue.mpg.de>
2022-10-01 23:34:13 +0200

    [ADD] Add CGInverseLinearOperator to API

--- curvlinops/__init__.py
+++ curvlinops/__init__.py
@@ -2,8 +2,10 @@
 
 from curvlinops.ggn import GGNLinearOperator
 from curvlinops.hessian import HessianLinearOperator
+from curvlinops.inverse import CGInverseLinearOperator
 
 __all__ = [
     "HessianLinearOperator",
     "GGNLinearOperator",
+    "CGInverseLinearOperator",
 ]
