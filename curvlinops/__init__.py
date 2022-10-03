05e1dcadf16d3eea3fb17524b2c35c277c385d29
Felix Dangel <fdangel@tue.mpg.de>
2022-10-03 14:29:28 +0200

    [DOC] Add spectral density methods to API

--- curvlinops/__init__.py
+++ curvlinops/__init__.py
@@ -3,9 +3,15 @@
 from curvlinops.ggn import GGNLinearOperator
 from curvlinops.hessian import HessianLinearOperator
 from curvlinops.inverse import CGInverseLinearOperator
+from curvlinops.papyan2020traces.spectrum import (
+    lanczos_approximate_log_spectrum,
+    lanczos_approximate_spectrum,
+)
 
 __all__ = [
     "HessianLinearOperator",
     "GGNLinearOperator",
     "CGInverseLinearOperator",
+    "lanzcos_approximate_spectrum",
+    "lanzcos_approximate_log_spectrum",
 ]
