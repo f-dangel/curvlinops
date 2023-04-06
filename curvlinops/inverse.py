891195af0cbbca77ede76076b78f4da3c240d8c6
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2023-04-06 17:28:11 +0200

    [ADD] Linear operator for Neumann inverse (#28)
    * [ADD] Implement `NeumannInverseLinearOperator` and test on toy problem
    
    * [ADD] Test Neumann inverse on damped GGN
    
    * [ADD] Modify truncation default, improve error message

--- curvlinops/inverse.py
+++ curvlinops/inverse.py
@@ -1,10 +1,25 @@
 """Implements linear operator inverses."""
 
-from numpy import column_stack, ndarray
+from numpy import allclose, column_stack, ndarray
 from scipy.sparse.linalg import LinearOperator, cg
 
 
-class CGInverseLinearOperator(LinearOperator):
+class _InverseLinearOperator(LinearOperator):
+    """Base class for (approximate) inverses of linear operators."""
+
+    def _matmat(self, X: ndarray) -> ndarray:
+        """Matrix-matrix multiplication.
+
+        Args:
+            X: Matrix for multiplication.
+
+        Returns:
+            Matrix-multiplication result ``A⁻¹@ X``.
+        """
+        return column_stack([self @ col for col in X.T])
+
+
+class CGInverseLinearOperator(_InverseLinearOperator):
     """Class for inverse linear operators via conjugate gradients."""
 
     def __init__(self, A: LinearOperator):
@@ -54,13 +69,131 @@ class CGInverseLinearOperator(LinearOperator):
         result, _ = cg(self._A, x, **self._cg_hyperparameters)
         return result
 
-    def _matmat(self, X: ndarray) -> ndarray:
-        """Matrix-matrix multiplication.
+
+class NeumannInverseLinearOperator(_InverseLinearOperator):
+    """Class for inverse linear operators via truncated Neumann series.
+
+    # noqa: B950
+
+    See https://en.wikipedia.org/w/index.php?title=Neumann_series&oldid=1131424698#Approximate_matrix_inversion.
+
+    Motivated by (referred to as lorraine2020optimizing in the following)
+
+    - Lorraine, J., Vicol, P., & Duvenaud, D. (2020). Optimizing millions of
+      hyperparameters by implicit differentiation. In International Conference on
+      Artificial Intelligence and Statistics (AISTATS).
+
+    .. warning::
+        The Neumann series can be non-convergent. In this case, the iterations
+        will become numerically unstable, leading to ``NaN``s.
+
+    .. warning::
+        The Neumann series can converge slowly.
+        Use :py:class:`curvlinops.CGInverLinearOperator` for better accuracy.
+    """
+
+    DEFAULT_NUM_TERMS = 100
+    DEFAULT_SCALE = 1.0
+    DEFAULT_CHECK_NAN = True
+
+    def __init__(
+        self,
+        A: LinearOperator,
+        num_terms: int = DEFAULT_NUM_TERMS,
+        scale: float = DEFAULT_SCALE,
+        check_nan: bool = DEFAULT_CHECK_NAN,
+    ):
+        r"""Store the linear operator whose inverse should be represented.
+
+        The Neumann series for an invertible linear operator :math:`\mathbf{A}` is
+
+        .. math::
+            \mathbf{A}^{-1}
+            =
+            \sum_{k=0}^{\infty}
+            \left(\mathbf{I} - \mathbf{A} \right)^k\,.
+
+        and is convergent if all eigenvalues satisfy
+        :math:`0 < \lambda(\mathbf{A}) < 2`.
+
+        By re-rescaling the matrix by ``scale`` (:math:`\alpha`), we have:
+
+        .. math::
+            \mathbf{A}^{-1}
+            =
+            \alpha (\alpha \mathbf{A})^{-1}
+            =
+            \alpha \sum_{k=0}^{\infty}
+            \left(\mathbf{I} - \alpha \mathbf{A} \right)^k\,,
+
+        which and is convergent if :math:`0 < \lambda(\mathbf{A}) < \frac{2}{\alpha}`.
+
+        Additionally, we truncate the series at ``num_terms`` (:math:`K`):
+
+        .. math::
+            \mathbf{A}^{-1}
+            \approx
+            \alpha \sum_{k=0}^{K}
+            \left(\mathbf{I} - \alpha \mathbf{A} \right)^k\,.
 
         Args:
-            X: Matrix for multiplication.
+            A: Linear operator whose inverse is formed.
+            num_terms: Number of terms in the truncated Neumann series.
+                Default: ``100``.
+            scale: Scale applied to the matrix in the Neumann iteration. Crucial
+                for convergence of Neumann series (details above). Default: ``1.0``.
+            check_nan: Whether to check for NaNs while applying the truncated Neumann
+                series. Default: ``True``.
+        """
+        super().__init__(A.dtype, A.shape)
+        self._A = A
+        self.set_neumann_hyperparameters(
+            num_terms=num_terms, scale=scale, check_nan=check_nan
+        )
+
+    def set_neumann_hyperparameters(
+        self,
+        num_terms: int = DEFAULT_NUM_TERMS,
+        scale: float = DEFAULT_SCALE,
+        check_nan: bool = DEFAULT_CHECK_NAN,
+    ):
+        """Store hyperparameters for the truncated Neumann series.
+
+        Args:
+            num_terms: Number of terms in the truncated Neumann series.
+                Default: ``100``.
+            scale: Scale applied to the matrix in the Neumann iteration. Crucial
+                for convergence of Neumann series. Default: ``1.0``.
+            check_nan: Whether to check for NaNs while applying the truncated Neumann
+                series. Default: ``True``.
+        """
+        self._num_terms = num_terms
+        self._scale = scale
+        self._check_nan = check_nan
+
+    def _matvec(self, x: ndarray) -> ndarray:
+        """Multiply x by the inverse of A.
+
+        Args:
+             x: Vector for multiplication.
 
         Returns:
-            Matrix-multiplication result ``A⁻¹@ X``.
+             Result of inverse matrix-vector multiplication, ``A⁻¹ @ x``.
+
+        Raises:
+            ValueError: If ``NaN`` check is turned on and ``NaN``s are detected.
         """
-        return column_stack([self @ col for col in X.T])
+        result, v = x.copy(), x.copy()
+
+        for idx in range(self._num_terms):
+            v = v - self._scale * (self._A @ v)
+            result = result + v
+
+            if self._check_nan and not allclose(result, result):
+                raise ValueError(
+                    f"Detected NaNs after application of {idx}-th term."
+                    + " This is probably because the Neumann series is non-convergent."
+                    + " Try decreasing `scale` and read the comment on convergence."
+                )
+
+        return self._scale * result
