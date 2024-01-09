054fa434a1a57e7618ed6bfd7ca1d7a885391441
runame <re393@cam.ac.uk>
2024-01-09 22:37:57 +0100

    Add weight-sharing KFAC test cases

--- test/kfac_cases.py
+++ test/kfac_cases.py
@@ -1,14 +1,20 @@
 """Contains test cases for the KFAC linear operator."""
 
 from functools import partial
-from test.utils import classification_targets, get_available_devices, regression_targets
+from test.utils import (
+    WeightShareModel,
+    classification_targets,
+    get_available_devices,
+    regression_targets,
+)
 
 from torch import rand
 from torch.nn import CrossEntropyLoss, Linear, MSELoss, Sequential
 
+
 # Add test cases here, devices and loss function with different reductions will be
 # added automatically below
-KFAC_EXPAND_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC = [
+KFAC_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC = [
     ###############################################################################
     #                                  REGRESSION                                 #
     ###############################################################################
@@ -32,8 +38,8 @@ KFAC_EXPAND_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC = [
     },
 ]
 
-KFAC_EXPAND_EXACT_CASES = []
-for case in KFAC_EXPAND_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC:
+KFAC_EXACT_CASES = []
+for case in KFAC_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC:
     for device in get_available_devices():
         for reduction in ["mean", "sum"]:
             case_with_device_and_loss_func = {
@@ -41,11 +47,50 @@ for case in KFAC_EXPAND_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC:
                 "device": device,
                 "loss_func": partial(MSELoss, reduction=reduction),
             }
-            KFAC_EXPAND_EXACT_CASES.append(case_with_device_and_loss_func)
+            KFAC_EXACT_CASES.append(case_with_device_and_loss_func)
+
+
+# Add test cases here, devices and loss function with different reductions will be
+# added automatically below
+KFAC_WEIGHT_SHARING_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC = [
+    ###############################################################################
+    #                                  REGRESSION                                 #
+    ###############################################################################
+    # deep linear network with vector output and weight-sharing dimensions
+    {
+        "model_func": lambda: WeightShareModel(Linear(5, 4), Linear(4, 3)),
+        "data": lambda: {
+            # exactess with expand requires that the product of all
+            # weight-sharing dimension sizes is the same in all batches
+            # (e.g. 32 = 4 * 8)
+            "expand": [
+                (rand(2, 32, 5), regression_targets((2, 32, 3))),
+                (rand(7, 4, 8, 5), regression_targets((7, 4, 8, 3))),
+            ],
+            # for reduce it works with arbitrary weight-sharing dims per batch
+            "reduce": [
+                (rand(1, 4, 5), regression_targets((1, 3))),
+                (rand(7, 4, 8, 5), regression_targets((7, 3))),
+            ],
+        },
+        "seed": 0,
+    },
+]
+
+KFAC_WEIGHT_SHARING_EXACT_CASES = []
+for case in KFAC_WEIGHT_SHARING_EXACT_CASES_NO_DEVICE_NO_LOSS_FUNC:
+    for device in get_available_devices():
+        for reduction in ["mean", "sum"]:
+            case_with_device_and_loss_func = {
+                **case,
+                "device": device,
+                "loss_func": partial(MSELoss, reduction=reduction),
+            }
+            KFAC_WEIGHT_SHARING_EXACT_CASES.append(case_with_device_and_loss_func)
 
 
 # Add test cases here, devices will be added automatically below
-KFAC_EXPAND_EXACT_ONE_DATUM_CASES_NO_DEVICE = [
+KFAC_EXACT_ONE_DATUM_CASES_NO_DEVICE = [
     ###############################################################################
     #                                CLASSIFICATION                               #
     ###############################################################################
@@ -64,11 +109,11 @@ KFAC_EXPAND_EXACT_ONE_DATUM_CASES_NO_DEVICE = [
     },
 ]
 
-KFAC_EXPAND_EXACT_ONE_DATUM_CASES = []
-for case in KFAC_EXPAND_EXACT_ONE_DATUM_CASES_NO_DEVICE:
+KFAC_EXACT_ONE_DATUM_CASES = []
+for case in KFAC_EXACT_ONE_DATUM_CASES_NO_DEVICE:
     for device in get_available_devices():
         case_with_device = {
             **case,
             "device": device,
         }
-        KFAC_EXPAND_EXACT_ONE_DATUM_CASES.append(case_with_device)
+        KFAC_EXACT_ONE_DATUM_CASES.append(case_with_device)
