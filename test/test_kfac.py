ec044b2f2092739653398478d26246aba9d40827
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-03-27 12:25:54 -0400

    [FIX] Slightly bump rtol for KFAC logdet test

--- test/test_kfac.py
+++ test/test_kfac.py
@@ -719,7 +719,7 @@ def test_logdet(case, exclude, separate_weight_and_bias, check_deterministic):
     assert not isinf(log_det) and not isnan(log_det)
     sign, logabsdet = slogdet(kfac @ eye(kfac.shape[1]))
     log_det_naive = sign * logabsdet
-    report_nonclose(log_det.cpu().numpy(), log_det_naive)
+    report_nonclose(log_det.cpu().numpy(), log_det_naive, rtol=2e-5)
 
     # Check that the logdet property is properly cached and reset
     assert kfac._logdet == log_det
