[FIX] Scaling of empirical/MC Fisher for output with more than two dimensions (#109)
* Fix EF scale for >2d outputs

* Fix scale of MC Fisher for >2d outputs

* Increase tolerance of CG test

* Increase CG precision

* Increase tolerance of CG test (again)

* Increase tolerance of CG test (again)

* Increase tolerance of CG test (again)

* Exclude numerically unstable cases from inverse tests

* Increase tolerance of Jacobian tests

* Adjust loss scale for KFAC-EF

* Add test for KFAC-MC with weight sharing

* Increase tolerance of KFAC (log)det tests

* Add Felix rewrite of functorch_empirical_fisher

* Increase EF test tolerance

* Fix tests

curvlinops/examples/functorch.py
@@ -2,12 +2,13 @@
 
 from collections.abc import MutableMapping
 from math import sqrt
-from typing import Callable, Dict, Iterable, List, Optional, Tuple, Union
+from typing import Dict, Iterable, List, Optional, Tuple, Union
 
 import torch
-from torch import Tensor, cat, einsum
-from torch.func import functional_call, grad, hessian, jacrev, jvp, vmap
-from torch.nn import Module
+from einops import rearrange
+from torch import Tensor, cat, stack
+from torch.func import functional_call, grad, hessian, jacrev, jvp
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, Module, MSELoss
 
 
 def blocks_to_matrix(blocks: Dict[str, Dict[str, Tensor]]) -> Tensor:
@@ -198,7 +199,6 @@ def functorch_empirical_fisher(
     loss_func: Module,
     params: List[Tensor],
     data: Iterable[Tuple[Union[Tensor, MutableMapping], Tensor]],
-    batch_size_fn: Optional[Callable[[MutableMapping], int]] = None,
     input_key: Optional[str] = None,
 ) -> Tensor:
     """Compute the empirical Fisher with functorch.
@@ -211,52 +211,70 @@ def functorch_empirical_fisher(
         params: List of differentiable parameters used by the prediction function.
         data: Source from which mini-batches can be drawn, for instance a list of
             mini-batches ``[(X, y), ...]`` or a torch ``DataLoader``.
-        batch_size_fn: Given an input ``X``, tells the batch size. When ``None``,
-            defaults to ``lambda X: X.shape[0]``.
         input_key: Key to obtain the input tensor when ``X`` is a dict-like object.
 
     Returns:
         Square matrix containing the empirical Fisher.
-
-    Raises:
-        ValueError: If the loss function's reduction cannot be determined.
     """
     (dev,) = {p.device for p in params}
     X, y = _concatenate_batches(data, input_key, device=dev)
     params_dict = _make_params_dict(model_func, params)
 
-    # compute batched gradients
-    def loss_n(
-        X_n: Union[Tensor, MutableMapping], y_n: Tensor, params_dict: Dict[str, Tensor]
+    def losses(
+        X: Union[Tensor, MutableMapping], y: Tensor, params_dict: Dict[str, Tensor]
     ) -> Tensor:
-        """Compute the gradient for a single sample.
+        """Compute all elementary losses without reduction.
 
-        # noqa: DAR101
-        # noqa: DAR201
+        An elementary loss results from a scalar entry of `y`.
+
+        Args:
+            X: Mini-batch input.
+            y: Mini-batch labels.
+            params_dict: Dictionary of parameters.
+
+        Returns:
+            1d tensor containing all elementary losses.
         """
-        output = functional_call(model_func, params_dict, X_n)
-        return functional_call(loss_func, {}, (output, y_n))
+        output = functional_call(model_func, params_dict, X)
+
+        flatten_output = {
+            MSELoss: "batch ... d_out -> (batch ... d_out)",
+            BCEWithLogitsLoss: "batch ... d_out -> (batch ... d_out)",
+            CrossEntropyLoss: "batch c ... -> (batch ...) c",
+        }[loss_func.__class__]
+        flatten_y = "... -> (...)"
+        output_flat, y_flat = rearrange(output, flatten_output), rearrange(y, flatten_y)
+
+        return stack(
+            [
+                functional_call(loss_func, {}, (o_el, y_el))
+                for o_el, y_el in zip(output_flat, y_flat)
+            ]
+        )
 
     params_argnum = 2
-    batch_grad_fn = vmap(grad(loss_n, argnums=params_argnum))
-    N = X.shape[0] if batch_size_fn is None else batch_size_fn(X)
+    jac = jacrev(losses, argnums=params_argnum)(X, y, params_dict)
+    jac = cat([j.flatten(start_dim=1) for j in jac.values()], dim=1)
 
-    params_replicated_dict = {
-        name: p.unsqueeze(0).expand(N, *(p.dim() * [-1]))
-        for name, p in params_dict.items()
-    }
+    # the losses over which the expectation is taken
+    num_losses = {
+        CrossEntropyLoss: y.numel(),
+        MSELoss: y.shape[:-1].numel(),
+        BCEWithLogitsLoss: y.shape[:-1].numel(),
+    }[loss_func.__class__]
+    num_params = sum(p.numel() for p in params)
 
-    batch_grad = batch_grad_fn(X, y, params_replicated_dict)
-    batch_grad = cat([bg.flatten(start_dim=1) for bg in batch_grad.values()], dim=1)
+    # the losses which model the same random variable
+    grouped_losses = y.numel() // num_losses
+    jac = jac.reshape(num_losses, grouped_losses, num_params).sum(1)
+    if (
+        isinstance(loss_func, (MSELoss, BCEWithLogitsLoss))
+        and loss_func.reduction == "mean"
+    ):
+        jac /= sqrt(grouped_losses)
 
-    if loss_func.reduction == "sum":
-        normalization = 1
-    elif loss_func.reduction == "mean":
-        normalization = N
-    else:
-        raise ValueError("Cannot detect reduction method from loss function.")
-
-    return 1 / normalization * einsum("ni,nj->ij", batch_grad, batch_grad)
+    normalization = {"sum": 1, "mean": num_losses}[loss_func.reduction]
+    return jac.T @ jac / normalization
 
 
 def functorch_jacobian(

curvlinops/gradient_moments.py
@@ -3,12 +3,13 @@
 from __future__ import annotations
 
 from collections.abc import MutableMapping
-from typing import List, Tuple, Union
+from typing import Callable, Iterable, List, Optional, Tuple, Union
 
 from backpack.hessianfree.ggnvp import ggn_vector_product_from_plist
-from einops import einsum
+from einops import einsum, rearrange
 from torch import Tensor, zeros_like
 from torch.autograd import grad
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss, Parameter
 
 from curvlinops._base import _LinearOperator
 
@@ -45,6 +46,72 @@ class EFLinearOperator(_LinearOperator):
         inefficient for-loop.
     """
 
+    supported_losses = (MSELoss, CrossEntropyLoss, BCEWithLogitsLoss)
+
+    def __init__(
+        self,
+        model_func: Callable[[Tensor], Tensor],
+        loss_func: Union[Callable[[Tensor, Tensor], Tensor], None],
+        params: List[Parameter],
+        data: Iterable[Tuple[Union[Tensor, MutableMapping], Tensor]],
+        progressbar: bool = False,
+        check_deterministic: bool = True,
+        num_data: Optional[int] = None,
+        batch_size_fn: Optional[Callable[[MutableMapping], int]] = None,
+    ):
+        """Linear operator for the uncentered gradient covariance/empirical Fisher (EF).
+
+        Note:
+            f(X; θ) denotes a neural network, parameterized by θ, that maps a mini-batch
+            input X to predictions p. ℓ(p, y) maps the prediction to a loss, using the
+            mini-batch labels y.
+
+        Args:
+            model_func: A function that maps the mini-batch input X to predictions.
+                Could be a PyTorch module representing a neural network.
+            loss_func: Loss function criterion. Maps predictions and mini-batch labels
+                to a scalar value.
+            params: List of differentiable parameters used by the prediction function.
+            data: Source from which mini-batches can be drawn, for instance a list of
+                mini-batches ``[(X, y), ...]`` or a torch ``DataLoader``. Note that ``X``
+                could be a ``dict`` or ``UserDict``; this is useful for custom models.
+                In this case, you must (i) specify the ``batch_size_fn`` argument, and
+                (ii) take care of preprocessing like ``X.to(device)`` inside of your
+                ``model.forward()`` function. Due to the sequential internal Monte-Carlo
+                sampling, batches must be presented in the same deterministic
+                order (no shuffling!).
+            progressbar: Show a progressbar during matrix-multiplication.
+                Default: ``False``.
+            check_deterministic: Probe that model and data are deterministic, i.e.
+                that the data does not use `drop_last` or data augmentation. Also, the
+                model's forward pass could depend on the order in which mini-batches
+                are presented (BatchNorm, Dropout). Default: ``True``. This is a
+                safeguard, only turn it off if you know what you are doing.
+            num_data: Number of data points. If ``None``, it is inferred from the data
+                at the cost of one traversal through the data loader.
+            batch_size_fn: If the ``X``'s in ``data`` are not ``torch.Tensor``, this
+                needs to be specified. The intended behavior is to consume the first
+                entry of the iterates from ``data`` and return their batch size.
+
+        Raises:
+             NotImplementedError: If the loss function differs from ``MSELoss``,
+                 BCEWithLogitsLoss, or ``CrossEntropyLoss``.
+        """
+        if not isinstance(loss_func, self.supported_losses):
+            raise NotImplementedError(
+                f"Loss must be one of {self.supported_losses}. Got: {loss_func}."
+            )
+        super().__init__(
+            model_func,
+            loss_func,
+            params,
+            data,
+            progressbar=progressbar,
+            check_deterministic=check_deterministic,
+            num_data=num_data,
+            batch_size_fn=batch_size_fn,
+        )
+
     def _matmat_batch(
         self, X: Union[Tensor, MutableMapping], y: Tensor, M_list: List[Tensor]
     ) -> Tuple[Tensor, ...]:
@@ -63,9 +130,24 @@ class EFLinearOperator(_LinearOperator):
             leading dimension of matrix columns.
         """
         output = self._model_func(X)
-        reduction_factor = {"mean": self._batch_size_fn(X), "sum": 1.0}[
-            self._loss_func.reduction
-        ]
+        # If >2d output we convert to an equivalent 2d output
+        if isinstance(self._loss_func, CrossEntropyLoss):
+            output = rearrange(output, "batch c ... -> (batch ...) c")
+            y = rearrange(y, "batch ... -> (batch ...)")
+        else:
+            output = rearrange(output, "batch ... c -> (batch ...) c")
+            y = rearrange(y, "batch ... c -> (batch ...) c")
+
+        # Adjust the scale depending on the loss reduction used
+        num_loss_terms, C = output.shape
+        reduction_factor = {
+            "mean": (
+                num_loss_terms
+                if isinstance(self._loss_func, CrossEntropyLoss)
+                else num_loss_terms * C
+            ),
+            "sum": 1.0,
+        }[self._loss_func.reduction]
 
         # compute ∂ℓₙ/∂fₙ without reduction factor of L
         (grad_output,) = grad(self._loss_func(output, y), output)