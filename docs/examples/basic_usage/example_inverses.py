9b784706b5f204ed32c5f033cf4848f1361f014b
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-11-29 12:58:47 -0500

    [FIX] Resolve merge conflict artifact

--- docs/examples/basic_usage/example_inverses.py
+++ docs/examples/basic_usage/example_inverses.py
@@ -80,12 +80,8 @@ loss_function = nn.MSELoss(reduction="mean").to(DEVICE)
 # First,  we set up a linear operator for the damped GGN/Fisher
 
 data = [(X1, y1), (X2, y2)]
-<<<<<<< HEAD
-GGN = GGNLinearOperator(model, loss_function, params, data).to_scipy()
-=======
 GGN_torch = GGNLinearOperator(model, loss_function, params, data)
 GGN = GGN_torch.to_scipy()
->>>>>>> main
 
 delta = 1e-2
 damping = aslinearoperator(delta * sparse.eye(GGN.shape[0]))
