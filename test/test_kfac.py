487054fd7b0063f08b8ac6ed8a86c2a5a3b5f6ea
Felix Dangel <felix.dangel@vectorinstitute.ai>
2025-02-05 19:51:00 +0100

    [FIX] More `weights_only=False`

--- test/test_kfac.py
+++ test/test_kfac.py
@@ -1126,7 +1126,7 @@ def test_save_and_load_state_dict():
         [(X, y)],
     )
     with raises(ValueError, match="loss"):
-        kfac_new.load_state_dict(load("kfac_state_dict.pt"))
+        kfac_new.load_state_dict(load("kfac_state_dict.pt", weights_only=False))
 
     # create new KFAC with different loss reduction and try to load state dict
     kfac_new = KFACLinearOperator(
@@ -1136,7 +1136,7 @@ def test_save_and_load_state_dict():
         [(X, y)],
     )
     with raises(ValueError, match="reduction"):
-        kfac_new.load_state_dict(load("kfac_state_dict.pt"))
+        kfac_new.load_state_dict(load("kfac_state_dict.pt", weights_only=False))
 
     # create new KFAC with different model and try to load state dict
     wrong_model = Sequential(Linear(D_in, 10), ReLU(), Linear(10, D_out))
@@ -1148,7 +1148,7 @@ def test_save_and_load_state_dict():
         [(X, y)],
     )
     with raises(RuntimeError, match="loading state_dict"):
-        kfac_new.load_state_dict(load("kfac_state_dict.pt"))
+        kfac_new.load_state_dict(load("kfac_state_dict.pt", weights_only=False))
 
     # create new KFAC and load state dict
     kfac_new = KFACLinearOperator(
@@ -1158,7 +1158,7 @@ def test_save_and_load_state_dict():
         [(X, y)],
         check_deterministic=False,  # turn off to avoid computing KFAC again
     )
-    kfac_new.load_state_dict(load("kfac_state_dict.pt"))
+    kfac_new.load_state_dict(load("kfac_state_dict.pt", weights_only=False))
     # clean up
     os.remove("kfac_state_dict.pt")
 
