2317450850c6940e276a8636604498501354d98b
Felix Dangel <felix.dangel@vectorinstitute.ai>
2025-10-01 09:40:34 +0100

    [ADD] Implement properties for KFAC inverse

--- curvlinops/inverse.py
+++ curvlinops/inverse.py
@@ -385,8 +385,6 @@ class KFACInverseLinearOperator(PyTorchLinearOperator):
             [tuple(s) for s in A._in_shape], [tuple(s) for s in A._out_shape]
         )
         self._A = A
-        self.device = A.device
-        self.dtype = A.dtype
         if use_heuristic_damping and use_exact_damping:
             raise ValueError("Either use heuristic damping or exact damping, not both.")
         if (use_heuristic_damping or use_exact_damping) and isinstance(damping, tuple):
@@ -726,3 +724,21 @@ class KFACInverseLinearOperator(PyTorchLinearOperator):
         )
         inv_kfac.load_state_dict(state_dict)
         return inv_kfac
+
+    @property
+    def dtype(self) -> dtype:
+        """Determine the linear operator's data type.
+
+        Returns:
+            The linear operator's dtype.
+        """
+        return self._A.dtype
+
+    @property
+    def device(self) -> device:
+        """Determine the device the linear operators is defined on.
+
+        Returns:
+            The linear operator's device.
+        """
+        return self._A.device
