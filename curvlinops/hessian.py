0afcc65e668d6ead7cbd7bfcdce41b7d951e00d1
Felix Dangel <felix.dangel@vectorinstitute.ai>
2025-10-21 14:44:59 -0400

    [REF] Implement Hessian multiplication with `torch.func` [no ci]

--- curvlinops/hessian.py
+++ curvlinops/hessian.py
@@ -1,14 +1,109 @@
 """Contains a linear operator implementation of the Hessian."""
 
 from collections.abc import MutableMapping
-from typing import List, Union
+from functools import cached_property, partial
+from typing import Callable, List, Optional, Tuple, Union
 
-from backpack.hessianfree.hvp import hessian_vector_product
-from torch import Tensor, zeros_like
-from torch.autograd import grad
+from torch import Tensor, no_grad, vmap
+from torch.func import jacrev, jvp
+from torch.nn import Module, Parameter
 
 from curvlinops._torch_base import CurvatureLinearOperator
-from curvlinops.utils import split_list
+from curvlinops.utils import make_functional_model_and_loss, split_list
+
+
+def make_batch_hessian_matrix_product(
+    model_func: Module,
+    loss_func: Module,
+    params: Tuple[Parameter, ...],
+    block_sizes: Optional[List[int]] = None,
+) -> Callable[[Tensor, Tensor, Tuple[Tensor, ...]], Tuple[Tensor, ...]]:
+    r"""Set up function that multiplies the mini-batch Hessian onto a matrix in list format.
+
+    Args:
+        model_func: The neural network :math:`f_{\mathbf{\theta}}`.
+        loss_func: The loss function :math:`\ell`.
+        params: A tuple of parameters w.r.t. which the Hessian is computed.
+            All parameters must be part of ``model_func.parameters()``.
+        block_sizes: Sizes of parameter blocks for block-diagonal approximation.
+            If ``None``, the full Hessian is used.
+
+    Returns:
+        A function that takes inputs ``X``, ``y``, and a matrix ``M`` in list
+        format, and returns the mini-batch Hessian applied to ``M`` in list format.
+    """
+    # Determine block structure
+    block_sizes = [len(params)] if block_sizes is None else block_sizes
+
+    # Create block-specific functional calls: *block_params, X -> prediction
+    block_params = split_list(list(params), block_sizes)
+    block_functionals = []
+
+    for block in block_params:
+        # criterion functional c is the same for all blocks
+        f_block, c = make_functional_model_and_loss(model_func, loss_func, tuple(block))
+        block_functionals.append(f_block)
+
+    @no_grad()
+    def hessian_vector_product(
+        X: Tensor, y: Tensor, *v: Tuple[Tensor, ...]
+    ) -> Tuple[Tensor, ...]:
+        """Multiply the mini-batch Hessian on a vector in list format.
+
+        Args:
+            X: Input to the DNN.
+            y: Ground truth.
+            *v: Vector to be multiplied with in tensor list format.
+
+        Returns:
+            Result of Hessian multiplication in list format. Has the same shape as
+            ``v``, i.e. each tensor in the list has the shape of a parameter.
+        """
+        # Split input vectors by blocks
+        v_blocks = split_list(list(v), block_sizes)
+
+        # Set up loss functions for each block
+        block_grad_fns = []
+
+        def loss_fn(
+            f: Callable[[Tuple[Tensor, ...], Union[Tensor, MutableMapping]], Tensor],
+            *params: Tuple[Tensor, ...],
+        ) -> Tensor:
+            """Compute the mini-batch loss given the neural net and its parameters.
+
+            Args:
+                f: Functional model with signature (*params, X) -> prediction
+                *params: Parameters for the functional model.
+
+            Returns:
+                Mini-batch loss.
+            """
+            return c(f(*params, X), y)
+
+        for f_block, ps in zip(block_functionals, block_params):
+            # Define the loss function composition for this block
+            block_loss_fn = partial(loss_fn, f_block)
+            block_grad_fn = jacrev(block_loss_fn, argnums=tuple(range(len(ps))))
+            block_grad_fns.append(block_grad_fn)
+
+        # Compute the HVPs per block and concatenate the results
+        hvps = []
+        for grad_fn, ps, vs in zip(block_grad_fns, block_params, v_blocks):
+            _, hvp_block = jvp(grad_fn, tuple(ps), tuple(vs))
+            hvps.extend(hvp_block)
+
+        return tuple(hvps)
+
+    # Vectorize over vectors to multiply onto a matrix in list format
+    return vmap(
+        hessian_vector_product,
+        # No vmap in X, y, assume last axis is vmapped in the matrix list
+        in_dims=(None, None) + tuple(p.ndim for p in params),
+        # Vmapped output axis is last
+        out_dims=tuple(p.ndim for p in params),
+        # We want each vector to be multiplied with the same mini-batch Hessian
+        randomness="same",
+    )
 
 
 class HessianLinearOperator(CurvatureLinearOperator):
@@ -32,6 +127,33 @@ class HessianLinearOperator(CurvatureLinearOperator):
         \nabla_{\mathbf{\theta}}^2
         \ell(f_{\mathbf{\theta}}(\mathbf{x}_n), \mathbf{y}_n)\,.
 
+    Example:
+        >>> from torch import rand, eye, allclose, kron, manual_seed
+        >>> from torch.nn import Linear, MSELoss
+        >>> from curvlinops import HessianLinearOperator
+        >>>
+        >>> # Create a simple linear model without bias
+        >>> _ = manual_seed(0) # make deterministic
+        >>> D_in, D_out = 4, 2
+        >>> num_data, num_batches = 10, 3
+        >>> model = Linear(D_in, D_out, bias=False)
+        >>> params = list(model.parameters())
+        >>> loss_func = MSELoss(reduction='sum')
+        >>>
+        >>> # Generate synthetic dataset and chunk into batches
+        >>> X, y = rand(num_data, D_in),  rand(num_data, D_out)
+        >>> data = list(zip(X.split(num_batches), y.split(num_batches)))
+        >>>
+        >>> # Create Hessian linear operator
+        >>> H_op = HessianLinearOperator(model, loss_func, params, data)
+        >>>
+        >>> # Compare with the known Hessian matrix 2 I ⊗ Xᵀ X
+        >>> H_mat = 2 * kron(eye(D_out), X.T @ X)
+        >>> P = sum(p.numel() for p in params)
+        >>> v = rand(P) # generate a random vector
+        >>> (H_mat @ v).allclose(H_op @ v)
+        True
+
     Attributes:
         SUPPORTS_BLOCKS: Whether the linear operator supports block operations.
             Default is ``True``.
@@ -41,6 +163,24 @@ class HessianLinearOperator(CurvatureLinearOperator):
 
     SELF_ADJOINT: bool = True
     SUPPORTS_BLOCKS: bool = True
+
+    @cached_property
+    def _mp(
+        self,
+    ) -> Callable[
+        [Union[Tensor, MutableMapping], Tensor, Tuple[Tensor, ...]], Tuple[Tensor, ...]
+    ]:
+        """Lazy initialization of batch-Hessian matrix product function.
+
+        Returns:
+            Function that computes mini-batch Hessian-vector products, given inputs
+            ``X``, labels ``y``, and the entries ``v1, v2, ...`` of the vector in list
+            format. Produces a list of tensors with the same shape as the input vector
+            that represents the result of the batch-Hessian multiplication.
+        """
+        return make_batch_hessian_matrix_product(
+            self._model_func, self._loss_func, tuple(self._params), self._block_sizes
+        )
 
     def _matmat_batch(
         self, X: Union[Tensor, MutableMapping], y: Tensor, M: List[Tensor]
@@ -59,26 +199,4 @@ class HessianLinearOperator(CurvatureLinearOperator):
             ``M``, i.e. each tensor in the list has the shape of a parameter and a
             trailing dimension of matrix columns.
         """
-        loss = self._loss_func(self._model_func(X), y)
-
-        # Re-cycle first backward pass from the HVP's double-backward
-        grad_params = grad(loss, self._params, create_graph=True)
-
-        (num_vecs,) = {m.shape[-1] for m in M}
-        HM = [zeros_like(m) for m in M]
-
-        # per-block HMP
-        for M_block, p_block, g_block, HM_block in zip(
-            split_list(M, self._block_sizes),
-            split_list(self._params, self._block_sizes),
-            split_list(grad_params, self._block_sizes),
-            split_list(HM, self._block_sizes),
-        ):
-            for n in range(num_vecs):
-                col_n = hessian_vector_product(
-                    loss, p_block, [M[..., n] for M in M_block], grad_params=g_block
-                )
-                for p, col in enumerate(col_n):
-                    HM_block[p][..., n].add_(col)
-
-        return HM
+        return list(self._mp(X, y, *M))
