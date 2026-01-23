b332d25b4ec7fd9fb6014b7c790d282b3dd7cd4b
Felix Dangel <felix.dangel@vectorinstitute.ai>
2026-01-23 15:27:36 -0500

    [REF] Merge exact and MC tests

--- test/test_ggn.py
+++ test/test_ggn.py
@@ -1,6 +1,8 @@
 """Contains tests for ``curvlinops/ggn``."""
 
-from pytest import raises
+from typing import Dict
+
+from pytest import mark, raises
 from torch import Tensor
 from torch.nn import BCEWithLogitsLoss
 
@@ -29,19 +31,30 @@ def test_GGNLinearOperator_matvec(case, adjoint: bool, is_vec: bool):
     compare_matmat(G, G_mat, adjoint, is_vec, atol=1e-7, rtol=1e-4)
 
 
-def test_GGNDiagonalLinearOperator_matvec(case, adjoint: bool, is_vec: bool):
+DIAGONAL_CASES = [{"mode": "exact"}, {"mode": "mc", "mc_samples": 20_000}]
+DIAGONAL_IDS = [
+    "_".join(f"{k}_{v}" for k, v in case.items()) for case in DIAGONAL_CASES
+]
+
+
+@mark.parametrize("kwargs", DIAGONAL_CASES, ids=DIAGONAL_IDS)
+def test_GGNDiagonalLinearOperator_matvec(
+    case, adjoint: bool, is_vec: bool, kwargs: Dict
+):
     """Test matrix-matrix multiplication with the GGN diagonal.
 
     Args:
         case: Tuple of model, loss function, parameters, data, and batch size getter.
         adjoint: Whether to test the adjoint operator.
         is_vec: Whether to test matrix-vector or matrix-matrix multiplication.
+        kwargs: A dictionary containing additional keyword arguments for specifying how
+            the GGN diagonal is approximated (either exactly or via Monte-Carlo).
     """
     model_func, loss_func, params, data, batch_size_fn = case
 
     def _construct_G():
         return GGNDiagonalLinearOperator(
-            model_func, loss_func, params, data, batch_size_fn=batch_size_fn
+            model_func, loss_func, params, data, batch_size_fn=batch_size_fn, **kwargs
         )
 
     # Check that BCEWithLogitsLoss raises an error
@@ -73,57 +86,8 @@ def test_GGNDiagonalLinearOperator_matvec(case, adjoint: bool, is_vec: bool):
     )
 
     compare_consecutive_matmats(G, adjoint, is_vec)
-    compare_matmat(G, G_mat, adjoint, is_vec, atol=1e-7, rtol=1e-4)
-
-
-def test_MCGGNDiagonalLinearOperator_matvec(case, adjoint: bool, is_vec: bool):
-    """Test matrix-matrix multiplication with the MCGGN diagonal.
-
-    Args:
-        case: Tuple of model, loss function, parameters, data, and batch size getter.
-        adjoint: Whether to test the adjoint operator.
-        is_vec: Whether to test matrix-vector or matrix-matrix multiplication.
-    """
-    model_func, loss_func, params, data, batch_size_fn = case
-
-    def _construct_G():
-        return GGNDiagonalLinearOperator(
-            model_func,
-            loss_func,
-            params,
-            data,
-            batch_size_fn=batch_size_fn,
-            mode="mc",
-            mc_samples=20_000,
-        )
-
-    # Check that BCEWithLogitsLoss raises an error
-    if isinstance(loss_func, BCEWithLogitsLoss):
-        with raises(RuntimeError, match="BCEWithLogitsLoss does not support vmap."):
-            _construct_G()
-        return
-
-    # Check that non-tensor inputs raise an error
-    if any(not isinstance(X, Tensor) for (X, _) in data):
-        with raises(
-            RuntimeError, match="Only tensor-valued inputs are supported by vmap."
-        ):
-            _construct_G()
-        return
-
-    # Check that sequence-valued predictions are unsupported
-    if model_func(data[0][0]).ndim > 2:
-        with raises(RuntimeError, match="Sequence-valued predictions are unsupported."):
-            _construct_G()
-        return
-
-    G = _construct_G()
-    G_mat = (
-        functorch_ggn(model_func, loss_func, params, data, input_key="x")
-        .detach()
-        .diag()  # extract the diagonal
-        .diag()  # embed it into a matrix
-    )
-
-    compare_consecutive_matmats(G, adjoint, is_vec)
-    compare_matmat(G, G_mat, adjoint, is_vec, atol=1e-4, rtol=2e-2)
+    tols = {
+        "atol": {"exact": 1e-7, "mc": 1e-4}[kwargs["mode"]],
+        "rtol": {"exact": 1e-4, "mc": 2e-2}[kwargs["mode"]],
+    }
+    compare_matmat(G, G_mat, adjoint, is_vec, **tols)
