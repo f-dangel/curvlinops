[BUG] Try extending Hessian square root to >2d predictions

curvlinops/kfac_utils.py
@@ -6,7 +6,18 @@ from typing import Callable, Tuple, Union
 from einconv import index_pattern
 from einconv.utils import get_conv_paddings
 from einops import einsum, rearrange, reduce
-from torch import Generator, Tensor, as_tensor, diag, eye, normal, softmax, zeros
+from torch import (
+    Generator,
+    zeros_like,
+    Tensor,
+    as_tensor,
+    diag,
+    normal,
+    softmax,
+    zeros,
+    block_diag,
+    vmap,
+)
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from torch.nn.functional import one_hot, unfold
 from torch.nn.modules.utils import _pair
@@ -45,9 +56,10 @@ def loss_hessian_matrix_sqrt(
 
     Args:
         output_one_datum: The model's prediction on a single datum. Has shape
-            ``[1, C]`` where ``C`` is the number of classes (outputs of the neural
-            network).
+            ``[1, *, C]`` where ``C`` is the number of classes (outputs of the neural
+            network) and * is arbitrary (e.g. empty or sequence length).
         target_one_datum: The label of the single datum.
+            Has shape ``[1, *]`` (CE) or ``[1, *, C]`` (BCE, MSE).
         loss_func: The loss function.
         check_binary_if_BCEWithLogitsLoss: Whether to check if targets are binary
             for BCEWithLogitsLoss. Default: ``True``.
@@ -55,7 +67,7 @@ def loss_hessian_matrix_sqrt(
     Returns:
         The matrix square root
         :math:`\mathbf{S}` of the Hessian. Has shape
-        ``[C, C]`` and satisfies the relation
+        ``[*, C, *, C]`` and satisfies the relation
 
         .. math::
             \mathbf{S} \mathbf{S}^\top
@@ -121,46 +133,72 @@ def loss_hessian_matrix_sqrt(
         where the square root is applied element-wise.
 
     Raises:
-        ValueError: If the batch size is not one, or the output is not 2d.
+        ValueError: If the batch size is not one.
         NotImplementedError: If the loss function is not supported.
-        NotImplementedError: If the loss function is ``BCEWithLogitsLoss`` but the
-            target is not binary.
     """
-    if output_one_datum.ndim != 2 or output_one_datum.shape[0] != 1:
+    if output_one_datum.shape[0] != 1:
         raise ValueError(
-            f"Expected 'output_one_datum' to be 2d with shape [1, C], got "
-            f"{output_one_datum.shape}"
+            f"Expected output_one_datum to have batch size 1, got {output_one_datum.shape}."
         )
     if target_one_datum.shape[0] != 1:  # targets for 2d predictions are sometimes 1d
         raise ValueError(
-            "Expected 'target_one_datum' to have batch_size 1."
-            + f" Got {target_one_datum.shape}."
+            f"Expected target_one_datum to have batch_size 1, got {target_one_datum.shape}."
         )
-    output = output_one_datum.squeeze(0)
-    output_dim = output.numel()
+    output_dim = output_one_datum.numel()
+    # Construct the Hessian square root as matrix (w.r.t. the flattened outputs)
 
     if isinstance(loss_func, MSELoss):
         c = {"sum": 1.0, "mean": 1.0 / output_dim}[loss_func.reduction]
-        return eye(output_dim, device=output.device, dtype=output.dtype).mul_(
-            sqrt(2 * c)
+        hess_sqrt_flat = (
+            zeros_like(output_one_datum).fill_(sqrt(2 * c)).flatten().diag()
         )
 
     elif isinstance(loss_func, CrossEntropyLoss):
         c = 1.0
-        p = output_one_datum.softmax(dim=1).squeeze()
-        p_sqrt = p.sqrt()
-        return (diag(p_sqrt) - einsum(p, p_sqrt, "i, j -> i j")).mul_(sqrt(c))
+        # Output has shape [1, C, d1, d2, ...], flatten into [C, d1 * d2 * ...]
+        output_flat = output_one_datum.squeeze(0).unsqueeze(-1).flatten(start_dim=1)
+        p = output_flat.softmax(dim=0)
+
+        def hess_sqrt_element(p: Tensor) -> Tensor:
+            """Compute the Hessian square root for a single element of the sequence.
+
+            Args:
+                p: Vector of probabilities for a single sequence. Has shape ``[C]``.
+
+            Returns:
+                The Hessian square root matrix. Has shape ``[C, C]``.
+            """
+            p_sqrt = p.sqrt()
+            return (diag(p_sqrt) - einsum(p, p_sqrt, "i, j -> i j")).mul_(sqrt(c))
+
+        # Compute the per-element Hessian square root
+        blocks = list(vmap(hess_sqrt_element, in_dims=-1)(p))
+        # print(output_flat.shape)
+        # print([b.shape for b in blocks])
+        # print(output_one_datum.shape)
+        # This matrix is [d1 * d2 * ... * C, d1 * d2 * ... * C], but we
+        # need [C * d1 * d2 * ..., C * d1 * d2 * ...]
+        hess_sqrt_flat = block_diag(*blocks)
+        C, D = output_flat.shape
+        hess_sqrt_flat = hess_sqrt_flat.reshape(D, C, D, C)
+        hess_sqrt_flat = einsum(hess_sqrt_flat, "d1 c1 d2 c2 -> c1 d1 c2 d2").reshape(
+            C * D, C * D
+        )
 
     elif isinstance(loss_func, BCEWithLogitsLoss):
         if check_binary_if_BCEWithLogitsLoss:
             _check_binary_if_BCEWithLogitsLoss(target_one_datum, loss_func)
 
         c = {"sum": 1.0, "mean": 1.0 / output_dim}[loss_func.reduction]
-        p = output_one_datum.sigmoid().squeeze(0)
+        p = output_one_datum.flatten().sigmoid()
         hess_diag = sqrt(c) * (p * (1 - p)).sqrt()
-        return hess_diag.diag()
+        hess_sqrt_flat = hess_diag.diag()
     else:
         raise NotImplementedError(f"Loss function {loss_func} not supported.")
+
+    # Un-flatten the output dimensions
+    output_shape = output_one_datum.shape[1:]
+    return hess_sqrt_flat.reshape(*output_shape, *output_shape)
 
 
 def extract_patches(

test/test_ggn.py
@@ -3,13 +3,13 @@
 from typing import Dict
 
 from pytest import mark, raises
-from torch import Tensor
-from torch.nn import BCEWithLogitsLoss
+from torch import Tensor, float64
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss
 
 from curvlinops import GGNLinearOperator
 from curvlinops.examples.functorch import functorch_ggn
 from curvlinops.ggn import GGNDiagonalLinearOperator
-from test.utils import compare_consecutive_matmats, compare_matmat
+from test.utils import compare_consecutive_matmats, compare_matmat, cast_input
 
 
 def test_GGNLinearOperator_matvec(case, adjoint: bool, is_vec: bool):
@@ -52,6 +52,23 @@ def test_GGNDiagonalLinearOperator_matvec(
     """
     model_func, loss_func, params, data, batch_size_fn = case
 
+    # Convert to float64 for higher precision
+    dtype = float64  # use double precision for better numerical stability
+    model_func = model_func.to(dtype=dtype)
+    loss_func = loss_func.to(dtype=dtype)
+    params = [p.to(dtype=dtype) for p in params]
+    data = [
+        (
+            (cast_input(x, dtype), y)
+            if isinstance(loss_func, CrossEntropyLoss)
+            else (cast_input(x, dtype), y.to(dtype=dtype))
+        )
+        for x, y in data
+    ]
+
+    print(case)
+    print(loss_func.reduction)
+
     def _construct_G():
         return GGNDiagonalLinearOperator(
             model_func, loss_func, params, data, batch_size_fn=batch_size_fn, **kwargs
@@ -66,7 +83,7 @@ def test_GGNDiagonalLinearOperator_matvec(
         return
 
     # Check that sequence-valued predictions are unsupported
-    if model_func(data[0][0]).ndim > 2:
+    if model_func(data[0][0]).ndim > 2 and kwargs["mode"] == "mc":
         with raises(RuntimeError, match="Sequence-valued predictions are unsupported."):
             _construct_G()
         return